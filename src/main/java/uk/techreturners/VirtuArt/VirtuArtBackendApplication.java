a12c08ab1d087f56706a568e2cf3feb92fa8f96f
Tawanda Chabva <tchabva@gmail.com>
2025-05-21 17:01:36 +0100

    init commit

---
+++ src/main/java/uk/techreturners/VirtuArt/VirtuArtBackendApplication.java
@@ -0,0 +1,13 @@
+package uk.techreturners.VirtuArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuArtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtuArtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/techreturners/VirtuArt/VirtuArtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.techreturners.VirtuArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuArtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
