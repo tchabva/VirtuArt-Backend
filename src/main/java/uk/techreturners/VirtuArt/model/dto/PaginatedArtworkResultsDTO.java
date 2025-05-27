d9fb5872a57be45880fa53ab5a5a1c37bf027b77
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 23:31:29 +0100

    feat: add Predicates for the PaginatedArtworkResultsDTO to determine hasNext and hasPrevious

--- src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
@@ -3,6 +3,8 @@ package uk.techreturners.VirtuArt.model.dto;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 @Builder
 public record PaginatedArtworkResultsDTO(
@@ -12,6 +14,10 @@ public record PaginatedArtworkResultsDTO(
         Integer totalPages,
         Integer currentPage,
         Boolean hasNext,
-        Boolean hasPrevious
+        Boolean hasPrevious,
+        String nextUrl
 ) {
+
+    public static BiPredicate<Integer, Integer> checkHasNext = (cPage, tPage) -> cPage < tPage;
+    public static Predicate<Integer> checkHasPrevious = cPage -> cPage == 1;
 }
