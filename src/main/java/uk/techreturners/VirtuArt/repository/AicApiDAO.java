d3ef001effc0a834c3f5ef5316180e6751f7a41e
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 05:40:31 +0100

    feat: create the Art Insitute of Chicago DAO with the getArtworks method

---
+++ src/main/java/uk/techreturners/VirtuArt/repository/AicApiDAO.java
@@ -0,0 +1,33 @@
+package uk.techreturners.VirtuArt.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
+import uk.techreturners.VirtuArt.model.aicapi.AicApiResult;
+import uk.techreturners.VirtuArt.model.aicapi.AicArtwork;
+import uk.techreturners.VirtuArt.model.aicapi.AicArtworkResult;
+import uk.techreturners.VirtuArt.model.aicapi.AicArtworkSearchResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AicApiDAO {
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("https://api.artic.edu/api/v1/artworks")
+            .build();
+
+    public AicApiResult getArtworks() {
+        try {
+            return webClient
+                    .get()
+                    .uri("?fields=id,title,artist_title,date_display&limit=20&page=1")
+                    .retrieve()
+                    .bodyToMono(AicApiResult.class)
+                    .block();
+        } catch (Exception e) {
+            // TODO: Error handling
+            return null;
+        }
+    }
+
+}
