df75f1aceeaa46cb75ff94fc53d5d0aa499afc83
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 15:28:31 +0100

    feat: create the ArtworkDTO

---
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/ArtworkDTO.java
@@ -0,0 +1,18 @@
+package uk.techreturners.VirtuArt.model.dto;
+
+import java.util.List;
+
+public record ArtworkDTO(
+        String id,
+        String title,
+        String artist,
+        String date,
+        String displayMedium,
+        String imageUrl,
+        List<String> altImageUrls,
+        String description,
+        String origin,
+        String category,
+        String sourceMuseum
+) {
+}
