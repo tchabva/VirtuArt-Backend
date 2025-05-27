aa537a88bcdd55ff161969ab95ded8a91034cea2
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 18:06:46 +0100

    feat: add JsonProperty annotations to the AicPagination Record Class

--- src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicPagination.java
+++ src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicPagination.java
@@ -1,11 +1,13 @@
 package uk.techreturners.VirtuArt.model.aicapi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AicPagination(
         Integer total,
         Integer limit,
         Integer offset,
-        Integer total_pages,
-        Integer current_page,
-        String next_url
+        @JsonProperty("total_pages") Integer totalPages,
+        @JsonProperty("current_page") Integer currentPage,
+        @JsonProperty("next_url") String nextUrl
 ) {
 }
