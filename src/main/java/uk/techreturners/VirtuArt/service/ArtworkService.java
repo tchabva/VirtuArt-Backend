0004fa36dd7f5799e6413f728d83811d4da64ddd
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 06:15:18 +0100

    feat: add the getArtworks method to the Artwork Service

--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
@@ -1,4 +1,7 @@
 package uk.techreturners.VirtuArt.service;
 
+import uk.techreturners.VirtuArt.model.aicapi.AicApiResult;
+
 public interface ArtworkService {
+    AicApiResult getArtworks(String limit, String page);
 }
