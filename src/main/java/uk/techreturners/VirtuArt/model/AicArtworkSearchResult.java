3487534cc17fd633f1dd7be3dca9e41a6024f211
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 04:22:29 +0100

    feat: create the AIC Search Results model

---
+++ src/main/java/uk/techreturners/VirtuArt/model/AicArtworkSearchResult.java
@@ -0,0 +1,9 @@
+package uk.techreturners.VirtuArt.model;
+
+public record AicArtworkSearchResult(
+        Integer id,
+        String title,
+        String date_display,
+        String artist_title
+) {
+}
