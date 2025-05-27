1eaf962ff7da56bffb574348371feb210869f4d3
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 06:20:20 +0100

    feat: implementation of the getAicArtworks methods

--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkServiceImpl.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkServiceImpl.java
@@ -1,4 +1,18 @@
 package uk.techreturners.VirtuArt.service;
 
-public class ArtworkServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.techreturners.VirtuArt.model.aicapi.AicApiResult;
+import uk.techreturners.VirtuArt.repository.AicApiDAO;
+
+@Service
+public class ArtworkServiceImpl implements ArtworkService {
+
+    @Autowired
+    private AicApiDAO aicApiDAO;
+
+    @Override
+    public AicApiResult getAicArtworks(String limit, String page) {
+        return aicApiDAO.getArtworks(limit, page);
+    }
 }
