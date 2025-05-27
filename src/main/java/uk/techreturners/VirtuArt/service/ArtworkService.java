86ba166116d14bf346f79a83c5c7ac0ef2c05082
Tawanda Chabva <tchabva@gmail.com>
2025-05-28 00:25:09 +0100

    feat: update the ArtworkService interface and implementation
    Update the return type to PaginatedArtworkResultsDTO and remove the redunant mapper method

--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
@@ -2,9 +2,6 @@ package uk.techreturners.VirtuArt.service;
 
 import uk.techreturners.VirtuArt.model.dto.PaginatedArtworkResultsDTO;
 
-import java.util.List;
-
 public interface ArtworkService {
     PaginatedArtworkResultsDTO getAicArtworks(String limit, String page);
-
 }
--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkServiceImpl.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkServiceImpl.java
@@ -2,20 +2,18 @@ package uk.techreturners.VirtuArt.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.techreturners.VirtuArt.model.aicapi.AicArtworkResult;
 import uk.techreturners.VirtuArt.model.dto.PaginatedArtworkResultsDTO;
 import uk.techreturners.VirtuArt.repository.AicApiDAO;
-
-import java.util.List;
+import uk.techreturners.VirtuArt.util.DTOMapper;
 
 @Service
-public class ArtworkServiceImpl implements ArtworkService {
+public class ArtworkServiceImpl implements ArtworkService, DTOMapper {
 
     @Autowired
     private AicApiDAO aicApiDAO;
 
     @Override
     public PaginatedArtworkResultsDTO getAicArtworks(String limit, String page) {
-        return aicApiDAO.getArtworks(limit, page);
+        return aicPaginatedResponseMapper(aicApiDAO.getArtworks(limit, page));
     }
 }
