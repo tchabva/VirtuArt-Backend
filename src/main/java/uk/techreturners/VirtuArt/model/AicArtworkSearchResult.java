c226300c8f31b982525761c83fa8d98d916b31f4
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 04:53:43 +0100

    feat: add JsonProperty annotation and update the attribute names

--- src/main/java/uk/techreturners/VirtuArt/model/AicArtworkSearchResult.java
+++ src/main/java/uk/techreturners/VirtuArt/model/AicArtworkSearchResult.java
@@ -1,9 +1,11 @@
 package uk.techreturners.VirtuArt.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AicArtworkSearchResult(
         Integer id,
         String title,
-        String date_display,
-        String artist_title
+        @JsonProperty("date_display") String dateDisplay,
+        @JsonProperty("artist_title") String artistTitle
 ) {
 }
