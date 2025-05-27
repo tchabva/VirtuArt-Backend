6a47d488683657b8c410ed6382a1950b763c195e
Tawanda Chabva <tchabva@gmail.com>
2025-05-27 06:17:51 +0100

    refactor: update the method name to getAicArtworks in the ArtworkService

--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
@@ -3,5 +3,5 @@ package uk.techreturners.VirtuArt.service;
 import uk.techreturners.VirtuArt.model.aicapi.AicApiResult;
 
 public interface ArtworkService {
-    AicApiResult getArtworks(String limit, String page);
+    AicApiResult getAicArtworks(String limit, String page);
 }
