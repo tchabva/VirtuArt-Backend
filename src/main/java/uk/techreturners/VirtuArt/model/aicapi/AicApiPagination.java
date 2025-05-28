3aa39aba35d1edc783dfa24572ba6119bb369dc3
Tawanda Chabva <tchabva@gmail.com>
2025-05-28 16:44:48 +0100

    feat: remove unused attributes from the AicApiPagination record model class

--- src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicApiPagination.java
+++ src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicApiPagination.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record AicApiPagination(
         Integer total,
         Integer limit,
-        Integer offset,
         @JsonProperty("total_pages") Integer totalPages,
-        @JsonProperty("current_page") Integer currentPage,
-        @JsonProperty("next_url") String nextUrl
+        @JsonProperty("current_page") Integer currentPage
 ) {
 }
