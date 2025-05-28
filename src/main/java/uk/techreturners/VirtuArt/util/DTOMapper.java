65a8b8a48383325cf6b699db71f2a7e8cf7d8c19
Tawanda Chabva <tchabva@gmail.com>
2025-05-29 00:43:57 +0100

    feat: implement exception handling for the DTOMapper methods

--- src/main/java/uk/techreturners/VirtuArt/util/DTOMapper.java
+++ src/main/java/uk/techreturners/VirtuArt/util/DTOMapper.java
@@ -1,5 +1,6 @@
 package uk.techreturners.VirtuArt.util;
 
+import uk.techreturners.VirtuArt.exception.ItemNotFoundException;
 import uk.techreturners.VirtuArt.model.aicapi.AicApiSearchResult;
 import uk.techreturners.VirtuArt.model.aicapi.AicApiSearchArtwork;
 import uk.techreturners.VirtuArt.model.aicapi.AicApiArtwork;
@@ -10,26 +11,30 @@ import uk.techreturners.VirtuArt.model.dto.PaginatedArtworkResultsDTO;
 // DTO Mapper Interface
 public interface DTOMapper {
     default ArtworkDTO createArtworkDtoWithAicApi(AicApiArtwork aicApiArtwork) {
-        return ArtworkDTO.builder()
-                .id(aicApiArtwork.id().toString())
-                .title(aicApiArtwork.title())
-                .artist(aicApiArtwork.artist())
-                .date(aicApiArtwork.dateDisplay())
-                .displayMedium(aicApiArtwork.displayMedium())
-                .imageUrl(aicImageUrlCreator(aicApiArtwork.primaryImageId()))
-                .altImageUrls( // Map each altImageId in the List to the URL
-                        aicApiArtwork.altImageIds().stream()
-                                .map(this::aicImageUrlCreator)
-                                .toList())
-                .description(aicApiArtwork.description())
-                .origin(aicApiArtwork.origin())
-                .category(aicApiArtwork.department())
-                .sourceMuseum("Art Institute of Chicago")
-                .build();
+        if (aicApiArtwork == null) {
+            throw new ItemNotFoundException("Null response from AIC API");
+        } else {
+            return ArtworkDTO.builder()
+                    .id(aicApiArtwork.id().toString())
+                    .title(aicApiArtwork.title())
+                    .artist(aicApiArtwork.artist())
+                    .date(aicApiArtwork.dateDisplay())
+                    .displayMedium(aicApiArtwork.displayMedium())
+                    .imageUrl(aicImageUrlCreator(aicApiArtwork.primaryImageId()))
+                    .altImageUrls( // Map each altImageId in the List to the URL
+                            aicApiArtwork.altImageIds().stream()
+                                    .map(this::aicImageUrlCreator)
+                                    .toList())
+                    .description(aicApiArtwork.description())
+                    .origin(aicApiArtwork.origin())
+                    .category(aicApiArtwork.department())
+                    .sourceMuseum("Art Institute of Chicago")
+                    .build();
+        }
     }
 
     default String aicImageUrlCreator(String artworkID) {
-        if(artworkID == null || artworkID.isBlank()){
+        if (artworkID == null || artworkID.isBlank()) {
             return "";
         } else {
             String AIC_IMAGE_URL = "https://www.artic.edu/iiif/2/";
@@ -39,35 +44,43 @@ public interface DTOMapper {
     }
 
     default PaginatedArtworkResultsDTO aicPaginatedResponseMapper(AicApiSearchResult aicApiSearchResult) {
-        return PaginatedArtworkResultsDTO.builder()
-                .data(
-                        aicApiSearchResult.data().stream()
-                                .map(this::aicSearchArtworkResultsResponseMapper)
-                                .toList())
-                .totalItems(aicApiSearchResult.pagination().total())
-                .pageSize(aicApiSearchResult.pagination().limit())
-                .totalPages(aicApiSearchResult.pagination().totalPages())
-                .currentPage(aicApiSearchResult.pagination().currentPage())
-                .hasNext(
-                        PaginatedArtworkResultsDTO.checkHasNext.test(
-                                aicApiSearchResult.pagination().currentPage(),
-                                aicApiSearchResult.pagination().totalPages()
-                        )
-                )
-                .hasPrevious(
-                        PaginatedArtworkResultsDTO.checkHasPrevious.test(
-                                aicApiSearchResult.pagination().currentPage()
-                        )
-                )
-                .build();
+        if (aicApiSearchResult == null) {
+            throw new ItemNotFoundException("Null response from AIC API");
+        } else {
+            return PaginatedArtworkResultsDTO.builder()
+                    .data(
+                            aicApiSearchResult.data().stream()
+                                    .map(this::aicSearchArtworkResultsResponseMapper)
+                                    .toList())
+                    .totalItems(aicApiSearchResult.pagination().total())
+                    .pageSize(aicApiSearchResult.pagination().limit())
+                    .totalPages(aicApiSearchResult.pagination().totalPages())
+                    .currentPage(aicApiSearchResult.pagination().currentPage())
+                    .hasNext(
+                            PaginatedArtworkResultsDTO.checkHasNext.test(
+                                    aicApiSearchResult.pagination().currentPage(),
+                                    aicApiSearchResult.pagination().totalPages()
+                            )
+                    )
+                    .hasPrevious(
+                            PaginatedArtworkResultsDTO.checkHasPrevious.test(
+                                    aicApiSearchResult.pagination().currentPage()
+                            )
+                    )
+                    .build();
+        }
     }
 
     default ArtworkResultsDTO aicSearchArtworkResultsResponseMapper(AicApiSearchArtwork aicArtworkSearchResult) {
-        return ArtworkResultsDTO.builder()
-                .id(aicArtworkSearchResult.id().toString())
-                .title(aicArtworkSearchResult.title())
-                .artistTitle(aicArtworkSearchResult.artistTitle())
-                .date(aicArtworkSearchResult.dateDisplay())
-                .build();
+        if (aicArtworkSearchResult == null) {
+            throw new ItemNotFoundException("Null response from AIC API");
+        } else {
+            return ArtworkResultsDTO.builder()
+                    .id(aicArtworkSearchResult.id().toString())
+                    .title(aicArtworkSearchResult.title())
+                    .artistTitle(aicArtworkSearchResult.artistTitle())
+                    .date(aicArtworkSearchResult.dateDisplay())
+                    .build();
+        }
     }
 }
