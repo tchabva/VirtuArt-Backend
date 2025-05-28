31db0a1de75bd47691d37ddf2b5667de6ef98570
Tawanda Chabva <tchabva@gmail.com>
2025-05-28 21:41:29 +0100

    feat: add ApiServiceException implementation to GlobalExceptionHandler

--- src/main/java/uk/techreturners/VirtuArt/exception/GlobalExceptionHandler.java
+++ src/main/java/uk/techreturners/VirtuArt/exception/GlobalExceptionHandler.java
@@ -1,9 +1,21 @@
 package uk.techreturners.VirtuArt.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Common keys for response body
+    private static final String MESSAGE_UNKNOWN_SERVICE_ERROR = "Unknown service error";
 
+    @ExceptionHandler(ApiServiceException.class)
+    public ResponseEntity<Object> handleApiServiceError(ApiServiceException e) {
+        HttpStatus status = e.getStatus() != null ? e.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+        String message = e.getMessage() != null ? e.getMessage() : MESSAGE_UNKNOWN_SERVICE_ERROR;
+
+        return ResponseEntity.status(status).body(message);
+    }
 }
