1933ce55f6bf400b403e974059de68f05a19717a
Tawanda Chabva <tchabva@gmail.com>
2025-05-30 15:01:13 +0100

    feat: create the ExhibitionItem entity model class

---
+++ src/main/java/uk/techreturners/VirtuArt/model/ExhibitionItem.java
@@ -0,0 +1,32 @@
+package uk.techreturners.VirtuArt.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "exhibition_items")
+public class ExhibitionItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "api_id")
+    private String apiId;
+
+    private String title;
+
+    @Column(name = "artist_title")
+    private String artistTitle;
+
+    private String date;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private String source;
+}
