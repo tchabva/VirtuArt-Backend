bbd1732e2d5fd2175880b5d81fe0d345c8dc99c6
Tawanda Chabva <tchabva@gmail.com>
2025-05-31 00:53:40 +0100

    feat: implement methods in the ExhibitionService

--- src/main/java/uk/techreturners/VirtuArt/service/ExhibitionService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ExhibitionService.java
@@ -1,4 +1,19 @@
 package uk.techreturners.VirtuArt.service;
 
+import uk.techreturners.VirtuArt.model.ExhibitionItem;
+import uk.techreturners.VirtuArt.model.dto.ExhibitionDTO;
+import uk.techreturners.VirtuArt.model.dto.ExhibitionDetailDTO;
+import uk.techreturners.VirtuArt.model.request.AddArtworkRequest;
+import uk.techreturners.VirtuArt.model.request.CreateExhibitionRequest;
+import uk.techreturners.VirtuArt.model.request.UpdateExhibitionRequest;
+
 public interface ExhibitionService {
+
+    ExhibitionDTO getAllUserExhibitions();
+    ExhibitionDetailDTO getExhibitionById(String id);
+    ExhibitionDTO createUserExhibition(CreateExhibitionRequest request);
+    ExhibitionItem addArtworkToExhibition(String exhibitionId, AddArtworkRequest request);
+    Void removeArtworkFromExhibition(String exhibitionId, String artworkId);
+    Void deleteExhibition(String id);
+    ExhibitionDTO updateExhibitionDetails(String exhibitionId, UpdateExhibitionRequest request);
 }
