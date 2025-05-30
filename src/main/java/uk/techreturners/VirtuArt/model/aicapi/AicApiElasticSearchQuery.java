6da352273820387142308019742a5950c34b7ca0
Tawanda Chabva <tchabva@gmail.com>
2025-05-30 02:02:10 +0100

    feat: implement the AicAPiElasticSearchQuery class

--- src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicApiElasticSearchQuery.java
+++ src/main/java/uk/techreturners/VirtuArt/model/aicapi/AicApiElasticSearchQuery.java
@@ -1,5 +1,65 @@
 package uk.techreturners.VirtuArt.model.aicapi;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AicApiElasticSearchQuery {
-    // TODO
+    @JsonProperty("query")
+    private Query query;
+
+    @JsonProperty("sort")
+    private List<Map<String, Object>> sort;
+
+    @JsonProperty("fields")
+    private List<String> fields;
+
+    @JsonProperty("size")
+    private int size;
+
+    @JsonProperty("page")
+    private int page;
+
+
+    // Inner classes for query structure
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Query {
+        @JsonProperty("bool")
+        private BoolQuery bool;
+
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class BoolQuery {
+        @JsonProperty("must")
+        private List<Map<String, Object>> must;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "ElasticsearchQuery{" +
+                "query=" + query +
+                ", sort=" + sort +
+                ", fields=" + fields +
+                ", size=" + size +
+                ", page=" + page +
+                '}';
+    }
 }
