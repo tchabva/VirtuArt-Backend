b1f9e74979204e2de3b81b192fb28e01874c4060
Tawanda Chabva <tchabva@gmail.com>
2025-05-30 15:05:57 +0100

    feat: create the repository interfaces for Entity classes

---
+++ src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionRepository.java
@@ -0,0 +1,8 @@
+package uk.techreturners.VirtuArt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExhibitionRepository extends JpaRepository<ExhibitionRepository, String> {
+}
