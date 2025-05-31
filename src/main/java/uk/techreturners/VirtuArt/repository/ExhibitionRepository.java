17f836820428648011b9eb8f4ffa036e3289be0e
Tawanda Chabva <tchabva@gmail.com>
2025-05-31 11:56:27 +0100

    fix: correct the JpaRepository type to Exhibition

--- src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionRepository.java
+++ src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionRepository.java
@@ -2,7 +2,9 @@ package uk.techreturners.VirtuArt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import uk.techreturners.VirtuArt.model.Exhibition;
 
 @Repository
-public interface ExhibitionRepository extends JpaRepository<ExhibitionRepository, String> {
+public interface ExhibitionRepository extends JpaRepository<Exhibition, String> {
+
 }
