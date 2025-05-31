0dccb0f5e2471a59f274edf0330f7bf399fda34e
Tawanda Chabva <tchabva@gmail.com>
2025-05-31 12:08:35 +0100

    feat: add custom repository methods to teh Exhibition and User Repository

--- src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionRepository.java
+++ src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionRepository.java
@@ -3,8 +3,11 @@ package uk.techreturners.VirtuArt.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.techreturners.VirtuArt.model.Exhibition;
+import uk.techreturners.VirtuArt.model.User;
+
+import java.util.List;
 
 @Repository
 public interface ExhibitionRepository extends JpaRepository<Exhibition, String> {
-
+    List<Exhibition> findByUser(User user);
 }
