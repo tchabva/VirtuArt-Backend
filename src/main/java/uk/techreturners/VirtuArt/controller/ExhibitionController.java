69065f4e38fe7d4b01784e43e4edd5e69d639665
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 02:40:15 +0100

    feat: implement the createExhibition method

--- src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
+++ src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
@@ -3,10 +3,9 @@ package uk.techreturners.VirtuArt.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.techreturners.VirtuArt.model.dto.ExhibitionDTO;
+import uk.techreturners.VirtuArt.model.request.CreateExhibitionRequest;
 import uk.techreturners.VirtuArt.service.ExhibitionService;
 
 import java.util.List;
@@ -21,5 +20,10 @@ public class ExhibitionController {
     @GetMapping
     public ResponseEntity<List<ExhibitionDTO>> getUserExhibitions() {
         return new ResponseEntity<>(exhibitionService.getAllUserExhibitions(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ExhibitionDTO> createExhibition(@RequestBody CreateExhibitionRequest request){
+        return new ResponseEntity<>(exhibitionService.createUserExhibition(request), HttpStatus.CREATED);
     }
 }
