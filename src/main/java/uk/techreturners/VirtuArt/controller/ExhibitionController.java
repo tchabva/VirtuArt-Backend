0a012b4c5a135b0c866cbd5aa0da03b9e31f9384
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 02:12:39 +0100

    feat: create the ExhibitionController class

---
+++ src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
@@ -0,0 +1,10 @@
+package uk.techreturners.VirtuArt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/exhibitions")
+public class ExhibitionController {
+
+}
