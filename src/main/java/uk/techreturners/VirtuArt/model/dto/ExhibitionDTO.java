292b553b4e93f95b12776fb6de3aadb313c7ec11
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 22:44:18 +0100

    style: optimise imports and remove whitespaces

--- src/main/java/uk/techreturners/VirtuArt/model/dto/ExhibitionDTO.java
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/ExhibitionDTO.java
@@ -1,10 +1,8 @@
 package uk.techreturners.VirtuArt.model.dto;
 
 import lombok.Builder;
-import uk.techreturners.VirtuArt.model.ExhibitionItem;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Builder
 public record ExhibitionDTO(
