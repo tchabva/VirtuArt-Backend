3a21a5f754d959d21e243d5e46197b7385c031c1
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 23:55:29 +0100

    code: optimise imports

--- src/main/java/uk/techreturners/VirtuArt/service/ExhibitionItemService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ExhibitionItemService.java
@@ -2,7 +2,6 @@ package uk.techreturners.VirtuArt.service;
 
 import uk.techreturners.VirtuArt.model.ExhibitionItem;
 import uk.techreturners.VirtuArt.model.request.AddArtworkRequest;
-import uk.techreturners.VirtuArt.model.request.CreateExhibitionRequest;
 
 public interface ExhibitionItemService {
     ExhibitionItem addNewExhibitionItem(AddArtworkRequest request);
