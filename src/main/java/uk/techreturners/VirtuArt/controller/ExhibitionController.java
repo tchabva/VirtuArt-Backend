e0b51ca00b6cbf6889f8fe2bcaf095f6fc9ab663
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 03:59:50 +0100

    feat: implement the getExhibitionDetails method

--- src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
+++ src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.techreturners.VirtuArt.model.dto.ExhibitionDTO;
 import uk.techreturners.VirtuArt.model.dto.ExhibitionDetailDTO;
+import uk.techreturners.VirtuArt.model.request.AddArtworkRequest;
 import uk.techreturners.VirtuArt.model.request.CreateExhibitionRequest;
 import uk.techreturners.VirtuArt.service.ExhibitionService;
 
@@ -36,5 +37,13 @@ public class ExhibitionController {
     @GetMapping("/{id}")
     public ResponseEntity<ExhibitionDetailDTO> getExhibitionDetails(@PathVariable("id") String exhibitId){
         return new ResponseEntity<>(exhibitionService.getExhibitionById(exhibitId), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ExhibitionDTO> getExhibitionDetails(
+            @PathVariable("id") String exhibitId,
+            @RequestBody AddArtworkRequest request
+    ){
+        return new ResponseEntity<>(exhibitionService.addArtworkToExhibition(exhibitId, request), HttpStatus.OK);
     }
 }
