506f96d5afa8db2743af4feb8fc04f5743642ca2
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 04:16:43 +0100

    feat: add findByApiIdAndSource method to ExhibitionItemRepository

--- src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionItemRepository.java
+++ src/main/java/uk/techreturners/VirtuArt/repository/ExhibitionItemRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.techreturners.VirtuArt.model.ExhibitionItem;
 
+import java.util.Optional;
+
 @Repository
 public interface ExhibitionItemRepository extends JpaRepository<ExhibitionItem, String> {
+    Optional<ExhibitionItem> findByApiIdAndSource(String apiId, String source);
 }
