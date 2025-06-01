4728f864ca72c04e51099599a7f9dc4a68e1a1a0
Tawanda Chabva <tchabva@gmail.com>
2025-06-01 02:22:51 +0100

    feat: implement the getUserExhibitions method

--- src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
+++ src/main/java/uk/techreturners/VirtuArt/controller/ExhibitionController.java
@@ -1,10 +1,25 @@
 package uk.techreturners.VirtuArt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.techreturners.VirtuArt.model.dto.ExhibitionDTO;
+import uk.techreturners.VirtuArt.service.ExhibitionService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/exhibitions")
 public class ExhibitionController {
 
+    @Autowired
+    private ExhibitionService exhibitionService;
+
+    @GetMapping
+    public ResponseEntity<List<ExhibitionDTO>> getUserExhibitions() {
+        return new ResponseEntity<>(exhibitionService.getAllUserExhibitions(), HttpStatus.OK);
+    }
 }
