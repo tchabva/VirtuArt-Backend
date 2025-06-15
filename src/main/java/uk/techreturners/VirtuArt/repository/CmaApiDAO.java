4c2b6f811cb9408bc40323e81a3eb8e29f0a5081
Tawanda Chabva <tchabva@gmail.com>
2025-06-15 04:25:51 +0100

    feat: create the getArtworkById method in the CmaApiDAO

--- src/main/java/uk/techreturners/VirtuArt/repository/CmaApiDAO.java
+++ src/main/java/uk/techreturners/VirtuArt/repository/CmaApiDAO.java
@@ -9,6 +9,7 @@ import org.springframework.web.reactive.function.client.WebClientRequestExceptio
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import uk.techreturners.VirtuArt.exception.ApiServiceException;
 import uk.techreturners.VirtuArt.exception.ItemNotFoundException;
+import uk.techreturners.VirtuArt.model.cmaapi.CmaApiArtworkResult;
 import uk.techreturners.VirtuArt.model.cmaapi.CmaApiSearchResult;
 
 @Repository
@@ -18,7 +19,7 @@ public class CmaApiDAO {
     @Qualifier("clevelandApiWebClient")
     private WebClient webClient;
 
-    public CmaApiSearchResult getArtworks(Integer limit, Integer page){
+    public CmaApiSearchResult getArtworks(Integer limit, Integer page) {
         try {
             int skip = (page - 1) * limit;
             String params = "/?cc0&fields=id,title,creation_date,creators,images&limit=";
@@ -28,6 +29,23 @@ public class CmaApiDAO {
                     .uri(params + limit + "&skip=" + skip)
                     .retrieve()
                     .bodyToMono(CmaApiSearchResult.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            HttpStatus status = HttpStatus.valueOf(e.getStatusCode().value());
+            throw new ApiServiceException(e.getMessage(), status);
+        } catch (WebClientRequestException e) {
+            throw new ApiServiceException(e.getMessage());
+        }
+    }
+
+    public CmaApiArtworkResult getArtworkById(String artworkId) {
+        try {
+            String params = "?fields=id,title,creators,images,culture,creation_date,culture,description,alt_image_ids,technique,department,alternate_images";
+            return webClient
+                    .get()
+                    .uri("/" + artworkId + params)
+                    .retrieve()
+                    .bodyToMono(CmaApiArtworkResult.class)
                     .block();
         }catch (WebClientResponseException e) {
             HttpStatus status = HttpStatus.valueOf(e.getStatusCode().value());
