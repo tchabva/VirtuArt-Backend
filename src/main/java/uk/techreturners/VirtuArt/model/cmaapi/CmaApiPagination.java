06e92166000e9c10187e6f7b8b3b679852782e42
Tawanda Chabva <tchabva@gmail.com>
2025-06-15 05:49:43 +0100

    feat: add helper methods to the CmaApiPagination record

--- src/main/java/uk/techreturners/VirtuArt/model/cmaapi/CmaApiPagination.java
+++ src/main/java/uk/techreturners/VirtuArt/model/cmaapi/CmaApiPagination.java
@@ -4,10 +4,29 @@ public record CmaApiPagination(
         Integer total,
         CmaApiPaginationParameters parameters
 ) {
-    private record CmaApiPaginationParameters(
+    public boolean checkHasNext() {
+        Integer currentPage = parameters.calculateCurrentPage();
+        Integer totalPages = parameters.calculateTotalPages(total);
+        return currentPage < totalPages;
+    }
+
+    public boolean checkHasPrevious() {
+        Integer currentPage = parameters.calculateCurrentPage();
+        return currentPage > 1;
+    }
+
+    public record CmaApiPaginationParameters(
             Integer skip,
             Integer limit
-    ){
+    ) {
         // Note: to calculate the skip = currentPage-1 (from Pager or Pagination) * limit
+
+        public Integer calculateCurrentPage() {
+            return (skip / limit) + 1;
+        }
+
+        public Integer calculateTotalPages(Integer total) {
+            return (int) Math.ceil((double) total / limit);
+        }
     }
 }
