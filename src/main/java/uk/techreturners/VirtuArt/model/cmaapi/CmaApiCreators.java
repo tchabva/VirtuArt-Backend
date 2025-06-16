c4da9bde5dd234f8679c2d42d59f28c9729189c5
Tawanda Chabva <tchabva@gmail.com>
2025-06-16 02:12:54 +0100

    feat: override the description getter to return the artist name only

--- src/main/java/uk/techreturners/VirtuArt/model/cmaapi/CmaApiCreators.java
+++ src/main/java/uk/techreturners/VirtuArt/model/cmaapi/CmaApiCreators.java
@@ -3,4 +3,9 @@ package uk.techreturners.VirtuArt.model.cmaapi;
 public record CmaApiCreators(
         String description
 ) {
+    // Returns the artist's name
+    @Override
+    public String description() {
+        return description.replaceAll("\\s*\\([^)]+\\)$", "");
+    }
 }
