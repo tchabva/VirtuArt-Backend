0c07e725fb65f7416686ccf7ce3d4fd9fee7aae2
Tawanda Chabva <tchabva@gmail.com>
2025-06-21 05:22:17 +0100

    style: optimise imports

--- src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
@@ -3,9 +3,6 @@ package uk.techreturners.VirtuArt.model.dto;
 import lombok.Builder;
 
 import java.util.List;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 @Builder
 public record PaginatedArtworkResultsDTO(
