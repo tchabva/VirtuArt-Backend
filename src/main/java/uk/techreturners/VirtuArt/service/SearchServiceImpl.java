cb8fdb57627c5d54d62d5b988581b1d9449f0e2a
Tawanda Chabva <tchabva@gmail.com>
2025-06-27 23:38:56 +0100

    feat: create the SearchServiceImpl class with the SearchService methods

---
+++ src/main/java/uk/techreturners/VirtuArt/service/SearchServiceImpl.java
@@ -0,0 +1,49 @@
+package uk.techreturners.VirtuArt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.techreturners.VirtuArt.model.aicapi.AicApiElasticSearchQuery;
+import uk.techreturners.VirtuArt.model.dto.PaginatedArtworkResultsDTO;
+import uk.techreturners.VirtuArt.model.request.AdvancedSearchRequest;
+import uk.techreturners.VirtuArt.model.request.BasicSearchRequest;
+import uk.techreturners.VirtuArt.repository.AicApiDAO;
+import uk.techreturners.VirtuArt.repository.CmaApiDAO;
+import uk.techreturners.VirtuArt.util.DTOMapper;
+import uk.techreturners.VirtuArt.util.SearchRequestMapper;
+
+public class SearchServiceImpl implements SearchService, DTOMapper, SearchRequestMapper {
+
+    @Autowired
+    private AicApiDAO aicApiDAO;
+
+    @Autowired
+    private CmaApiDAO cmaApiDAO;
+
+    @Override
+    public PaginatedArtworkResultsDTO getArtworksByAdvancedSearchQuery(AdvancedSearchRequest searchQuery) {
+        if (searchQuery.source().equalsIgnoreCase("aic")) {
+            return getAicArtworksBySearchQuery(createAicAdvancedElasticQuery(searchQuery));
+        } else {
+            throw new IllegalArgumentException(
+                    "The source ".concat(searchQuery.source()).concat(" could not be matched")
+            );
+        }
+    }
+
+    @Override
+    public PaginatedArtworkResultsDTO getArtworksByBasicSearchQuery(BasicSearchRequest searchQuery) {
+        switch (searchQuery.source()) {
+            case "aic" -> {
+                return getAicArtworksBySearchQuery(createBasicElasticQuery(searchQuery));
+            }
+            case "cma" -> {
+                return cmaPaginatedResponseMapper(cmaApiDAO.basicSearchQuery(searchQuery));
+            }
+            case null, default -> throw new IllegalArgumentException("Invalid basicSearch: " + searchQuery);
+        }
+    }
+
+    @Override
+    public PaginatedArtworkResultsDTO getAicArtworksBySearchQuery(AicApiElasticSearchQuery elasticSearchQuery) {
+        return aicPaginatedResponseMapper(aicApiDAO.getArtworksByElasticSearchQuery(elasticSearchQuery));
+    }
+}
