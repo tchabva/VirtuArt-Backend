3bb49a3d7eda386f434eb2474d7bfd2926d2a84f
Tawanda Chabva <tchabva@gmail.com>
2025-07-04 13:21:10 +0100

    refactor: remove redundant search code from the artwork files

--- src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
+++ src/main/java/uk/techreturners/VirtuArt/service/ArtworkService.java
@@ -1,10 +1,7 @@
 package uk.techreturners.VirtuArt.service;
 
-import uk.techreturners.VirtuArt.model.aicapi.AicApiElasticSearchQuery;
 import uk.techreturners.VirtuArt.model.dto.ArtworkDTO;
 import uk.techreturners.VirtuArt.model.dto.PaginatedArtworkResultsDTO;
-import uk.techreturners.VirtuArt.model.request.AdvancedSearchRequest;
-import uk.techreturners.VirtuArt.model.request.BasicSearchRequest;
 
 public interface ArtworkService {
 
@@ -15,10 +12,4 @@ public interface ArtworkService {
     PaginatedArtworkResultsDTO getCmaArtworks(String limit, String page);
 
     ArtworkDTO getArtworkById(String source, String artworkId);
-
-    PaginatedArtworkResultsDTO getArtworksByAdvancedSearchQuery(AdvancedSearchRequest searchQuery);
-
-    PaginatedArtworkResultsDTO getArtworksByBasicSearchQuery(BasicSearchRequest searchQuery);
-
-    PaginatedArtworkResultsDTO getAicArtworksBySearchQuery(AicApiElasticSearchQuery elasticSearchQuery);
 }
