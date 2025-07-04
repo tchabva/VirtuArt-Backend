c72a2df9520fe0690eb017a2d9cacac182238e08
Tawanda Chabva <tchabva@gmail.com>
2025-07-04 22:54:49 +0100

    test: specify the Spring Boot profile

--- src/test/java/uk/techreturners/VirtuArt/VirtuArtBackendApplicationTests.java
+++ src/test/java/uk/techreturners/VirtuArt/VirtuArtBackendApplicationTests.java
@@ -2,12 +2,13 @@ package uk.techreturners.VirtuArt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VirtuArtBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
