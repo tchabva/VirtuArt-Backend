d72431e5ba58e09758fcdc40c4ae36c366997635
Tawanda Chabva <tchabva@gmail.com>
2025-07-04 22:01:27 +0100

    fix: return DTO back to a record and remove the builder annotation

--- src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/PaginatedArtworkResultsDTO.java
@@ -1,22 +1,14 @@
 package uk.techreturners.VirtuArt.model.dto;
 
-import lombok.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@Builder
-public class PaginatedArtworkResultsDTO {
-    private Integer totalItems;
-    private Integer pageSize;
-    private Integer totalPages;
-    private Integer currentPage;
-    private Boolean hasNext;
-    private Boolean hasPrevious;
-    @Builder.Default
-    private List<ArtworkResultsDTO> data = new ArrayList<>();
+public record PaginatedArtworkResultsDTO(
+        Integer totalItems,
+        Integer pageSize,
+        Integer totalPages,
+        Integer currentPage,
+        Boolean hasNext,
+        Boolean hasPrevious,
+        List<ArtworkResultsDTO> data
+) {
 }
