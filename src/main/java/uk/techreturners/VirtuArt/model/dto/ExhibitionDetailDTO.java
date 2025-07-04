0fd793fd29a102e5bece086a2576a6fea0f4d355
Tawanda Chabva <tchabva@gmail.com>
2025-07-04 22:02:04 +0100

    feat: remove the Builder annotation

--- src/main/java/uk/techreturners/VirtuArt/model/dto/ExhibitionDetailDTO.java
+++ src/main/java/uk/techreturners/VirtuArt/model/dto/ExhibitionDetailDTO.java
@@ -1,11 +1,8 @@
 package uk.techreturners.VirtuArt.model.dto;
 
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Builder
 public record ExhibitionDetailDTO(
         String id,
         String title,
